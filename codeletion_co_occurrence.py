c65c483eb302d89a691011a5582d46ff3292e61f
TANISHI <TM1322@IC.AC.UK>
2024-09-15 12:46:24 +0100

    cnv and mutation analysis

--- codeletion_co_occurrence.py
+++ codeletion_co_occurrence.py
@@ -101,3 +101,92 @@ sns.heatmap(
 )
 plt.title('Co-occurrence Matrix of CNV Combinations', weight='bold', fontsize=14)
 plt.show()
+
+#Save output 
+co_occurrence_df.to_csv('codeletion_cooccurrence_counts.csv', header = True)
+co_occurrence_props.to_csv('codeletion_cooccurrence_props.csv', header = True)
+
+# -------------------------------------------------------------------- Mutation Data Analysis -------------------------------------------------------------------- #
+
+#Define commonly mutated genes in GBM and extract patient data for each gene
+mutations_df = mutations_df[(mutations_df['Annotation_Status'] == "SUCCESS") & (mutations_df["Mutation_Status"] == "Somatic")] 
+GOI = ['TP53', 'PTPRD', 'PTEN', 'EGFR', 'ATRX', 'TERT', 'IDH1', 'IDH2']
+mutations_filtered = mutations_df[mutations_df['Hugo_Symbol'].isin(GOI)]
+
+# Create sets of patient IDs for each mutation
+mutation_sets_dict = {}
+for gene in GOI:
+    mutation_sets_dict[gene + '_mut'] = set(mutations_filtered[mutations_filtered['Hugo_Symbol'] == gene]['Tumor_Sample_Barcode'].unique())
+
+# Create combined set for patients with CDKN2A/B and MTAP deletions. Seems redundant to keep them separate in the co-occurrence matrices if they are almost always co-deleted.
+MTAP_codel_patients = codel_patients.intersection(MTAP_del_patients)
+
+# Combine the co-deletion/amplification sets with the mutation sets dictionary
+combined_sets_dict = {
+    'CDKN2A/B-MTAP codel': MTAP_codel_patients,
+    'PTPRD del': PTPRD_del_patients,
+    'EGFR amp': EGFR_amp_patients
+}
+combined_sets_dict.update(mutation_sets_dict) 
+
+# Compute the counts co-occurrence matrix 
+def compute_co_occurrence_matrix(sets_dict):
+    results = pd.DataFrame(index=sets_dict.keys(), columns=sets_dict.keys())
+    for row in sets_dict:
+        for col in sets_dict:
+            if row == col:
+                results.at[row, col] = len(sets_dict[row])
+            else:
+                intersection_count = len(sets_dict[row].intersection(sets_dict[col]))
+                results.at[row, col] = intersection_count
+    return results
+
+co_occurrence_mut_df = compute_co_occurrence_matrix(combined_sets_dict)
+
+# Compute the proportions co-occurrence matrix 
+def compute_co_occurrence_proportions(sets_dict):
+    results = pd.DataFrame(index=sets_dict.keys(), columns=sets_dict.keys())
+    for row in sets_dict:
+        for col in sets_dict:
+            if row == col:
+                results.at[row, col] = 100.0
+            else:
+                intersection_count = len(sets_dict[row].intersection(sets_dict[col]))
+                region_count = len(sets_dict[row])
+                results.at[row, col] = (intersection_count/region_count) * 100
+    return results
+
+co_occurrence_mut_props = compute_co_occurrence_proportions(combined_sets_dict)
+
+#Visualise proportions co-occurrence 
+plt.figure(figsize=(12, 10))
+sns.heatmap(
+    co_occurrence_mut_props.astype(float), 
+    annot=True,
+    cmap='YlGnBu',
+    fmt='.2f', 
+    cbar=True,
+    annot_kws={"size": 12}
+)
+plt.title('Co-occurrence Matrix of CNV and Mutations', weight='bold', fontsize=14)
+plt.savefig("co_occurrence_matrix_CNV_mut.pdf", format='pdf', bbox_inches='tight')
+plt.show()
+
+# Visualise proportions for CDKN2A/B Codel Patients only 
+co_occurrence_codel_mut_props = co_occurrence_mut_props.drop(['PTPRD del', 'EGFR amp', 'TP53_mut', 'PTPRD_mut', 'PTEN_mut', 'EGFR_mut', 'ATRX_mut', 'TERT_mut', 'IDH1_mut', 'IDH2_mut'], axis = 0)
+co_occurrence_codel_mut_df = co_occurrence_mut_df.drop(['PTPRD del', 'EGFR amp', 'TP53_mut', 'PTPRD_mut', 'PTEN_mut', 'EGFR_mut', 'ATRX_mut', 'TERT_mut', 'IDH1_mut', 'IDH2_mut'], axis = 0)
+
+co_occurrence_codel_mut_df.to_csv("CDKN2A_B_codel_mut_cooccurrence.csv", header = True)
+
+plt.figure(figsize=(12, 10))
+sns.heatmap(
+    co_occurrence_codel_mut_props.astype(float), 
+    annot=True,
+    cmap='YlGnBu',
+    fmt='.2f', 
+    cbar=True,
+    annot_kws={"size": 12}
+)
+plt.title('Co-occurrence Matrix of Mutations in CDKN2A/B Codel Patients', weight='bold', fontsize=14)
+plt.savefig("co_occurrence_matrix_codel_mut.pdf", format='pdf', bbox_inches='tight')
+plt.show()
